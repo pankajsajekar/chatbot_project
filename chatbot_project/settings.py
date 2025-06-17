d02fc51a156d65ce39fd09142ed66c5c7cbbec34
pankajsajekar <pankajsajekar789@gmail.com>
2025-06-17 08:26:34 +0530

    docker

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -21,16 +21,16 @@ from decouple import config
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1_wqs%jy6#@86^0fm)g0#p!#tqpju+&8zq7qv)ep#!%-+0m4th'
+SECRET_KEY =  config('SECRET_KEY', default='django-insecure-1_wqs%jy6#@86^0fm)g0#p!#tqpju+&8zq7qv)ep#!%-+0m4th')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = [
     "127.0.0.1", "*",
 ]
 
-# CORS configuration
+# CORS  configuration
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
@@ -113,10 +113,21 @@ WSGI_APPLICATION = 'chatbot_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config("DATABASE_ENGINE"),
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USER"),
+        'PASSWORD': config("DATABASE_PASSWORD"),
+        'HOST': config("DATABASE_HOST"),
+        'PORT': config("DATABASE_PORT"),
     }
 }
 
